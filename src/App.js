8ebfb24e829a15fad19e0bb049786743a4c8625f
Sohaib <sohaibbutt448506@gmail.com>
2022-07-25 09:50:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.state = { array: [] };
+    this.state = { object: {} };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+    this.setState((x) => ({
+      array: [event.target.value],
+    }));
+  };
+  componentDidMount() {
+    console.log("Hello I am Component did mount");
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleSubmit(event) {
+    console.log(
+      "🚀 ~ file: App.js ~ line 19 ~ App ~ handleSubmit ~ event",
+      this.state.array
+    );
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        {console.log("Component Re-render")}
+        <input type="submit" value="Submit"></input>
+      </form>
+    );
+  }
 }
-
 export default App;
